39b911b821d312ed70b305fbb1e68763d6ef3aaf
B Water <brennw47@gmail.com>
2024-04-26 13:10:03 -0600

    Added the node package manager/Updated the gitignore to ignore .ds_store/ and node_modules//Added a Dist folder for the generated readme/Added a readme file/Added some pseudo code to the index.js and used the require method to include variables for the packages I need/ Added pseudo code to the generateMarkdown.js
    Added the node package manager with npm i, added the packages to ignore in the gitignore file, added a dist folder for an output spot for the generated readme however the readme cannot be generated yet. Added a readme file which will be for the github page. Added some pseudo code to the index.js to outline what I have to do some more, and used the require method to include variables for the packages I need as well. Lastly added pseudo code to the generateMarkdown.js to show what this file will be used for.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
+// make const variables for questions to use as parameter in the init function
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
+// fs.writefile('README.md', generateMarkdown(data), (err) => {
 // TODO: Create a function to initialize app
 function init() {}
-
+// inquirer.prompts([
 // Function call to initialize app
 init();
