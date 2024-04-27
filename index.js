0abad36f9cabffc6fb36fbc7263165ddfdc4a8ac
B Water <brennw47@gmail.com>
2024-04-26 18:39:57 -0600

    Updated index.js to have questions for the prompt that will populate the README.
    Updated index.js to have questions for the prompt this prompt will be given to a file system method called writeFile which will be inside of the writeToFile function however this isnt written yet so the prompt does not populate anything.

--- index.js
+++ index.js
@@ -3,13 +3,61 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please provide a description of your project.",
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "Please provide a table of contents.",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Please provide installation instructions.",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Please provide usage information.",
+    },
+    {
+        type: "list",
+        name: "License",
+        message: "Please choose a license for your project.",
+        choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please provide contribution guidelines.",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Please provide test instructions.",
+    },
+    {
+        type: "input",
+        name: "Questions",
+        message: "Do you have any questions?",
+    },
+];
 // make const variables for questions to use as parameter in the init function
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 // fs.writefile('README.md', generateMarkdown(data), (err) => {
 // TODO: Create a function to initialize app
-function init() {}
-// inquirer.prompts([
+function init() {
+}
 // Function call to initialize app
 init();
